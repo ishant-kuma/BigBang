927ca2311c35c2a4011e59a094416c427a3910d6
ishant-kuma <ishant.kanini@gmail.com>
2023-05-28 22:59:25 +0530

    Initial commit

---
+++ Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BigBangAssessment.Models
+{
+    public class Room
+    {
+        [Key]
+        public int RoomId { get; set; }
+        public string? RoomNumber { get; set; }
+        public string? Type { get; set; }
+        public int Capacity { get; set; }
+
+        public bool Available { get; set; }
+        public int Price { get; set; }
+
+        public int HotelId { get; set; }
+        public Hotel? Hotel { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+using BigBangAssessment.Authentication;
+using BigBangAssessment.Models;
+using BigBangAssessment.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+// Add services to the container.
+
+// For Entity Framework
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(@"Data Source=LAPTOP-97LKALQG\SQLEXPRESS07;database=HotelRoom1;Integrated Security=True; TrustServerCertificate=True"));
+builder.Services.AddDbContext<HotelRoomDbContext>(options => options.UseSqlServer(@"Data Source=LAPTOP-97LKALQG\SQLEXPRESS07;database=HotelRoom1;Integrated Security=True; TrustServerCertificate=True"));
+builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+builder.Services.AddScoped<IHotelRepository, HotelRepository>();
+
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+builder.Services.AddScoped<IFilterCountRepository, FilterCountRepository>();
+
+
+// For Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Adding Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+// Adding Jwt Bearer
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = configuration["JWT:ValidAudience"],
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+    };
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication(); // will not be avaialable
+
+app.UseAuthorization();//by default, this code will be avaialbale
+
+app.MapControllers();
+
+app.Run();
